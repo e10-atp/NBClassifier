18c68a97cf75a0ecaf372957fdaef95709892117
e10-atp <52726603+e10-atp@users.noreply.github.com>
2019-08-05 21:07:30 -0400

    Add files via upload

--- Gap.py
+++ Gap.py
@@ -4,11 +4,16 @@ class Gap:
 
     @staticmethod
     def horizontalAvg(image):
+        splitImage = image.split('\n')
+        del splitImage[-1]
         gapList = list()
         flag = False
-        for i in image:
+        for i in splitImage:
+            if i.strip() == '':
+                continue
             count = 0
             prev = None
+            gapList.append(0)
             for c in i:
                 if c == ' ' and flag is False:
                     if prev != ' ':
@@ -20,10 +25,10 @@ class Gap:
                     continue
                 elif c != ' ' and flag is True:
                     flag = False
+                    gapList[-1] = count
                 else:
                     continue
                 prev = c
-            gapList.append(count)
         return numpy.mean(gapList)
 
     @staticmethod
@@ -35,8 +40,11 @@ class Gap:
         flag = False
         gapList = list()
         for c in range(0, lineSize):
+            if Gap.columnBlank(splitImage, c, lineCount):
+                continue
             count = 0
             prev = None
+            gapList.append(0)
             for i in range(0, lineCount):
                 if splitImage[i][c] == ' ' and flag is False:
                     if prev != ' ':
@@ -48,8 +56,16 @@ class Gap:
                     continue
                 elif splitImage[i][c] != ' ' and flag is True:
                     flag = False
+                    gapList[-1] = count
                 else:
                     continue
                 prev = c
-            gapList.append(count)
         return numpy.mean(gapList)
+
+    @staticmethod
+    def columnBlank(image, column, colSize):
+        flag = True
+        for i in range(0, colSize):
+            if image[i][column] != ' ':
+                flag = False
+        return flag

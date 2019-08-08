97a0125ecf40c874fb7a05f6eb4a0ca8997a128b
patrick2008 <44302635+patrick2008@users.noreply.github.com>
2019-08-08 10:45:11 -0400

    Add files via upload

--- percepNum.py
+++ percepNum.py
@@ -2,6 +2,8 @@ from Node import Node
 from Scan import Scan
 import os, math, random, collections
 from Regression import Regression
+from Gap import Gap
+from ScaleDown import ScaleDown
 
 class percepNum:
     
@@ -21,13 +23,11 @@ class percepNum:
                 self.cntY[node.label] += 1
 
     def perceptron(self, x, j, weightsList, numPass):#each feature is an array
-    	#try to replace i with x here
-        #e.g. feature1[i] should be the feature for x
-        xList, yList = Regression.makeLists(x.image)
-        m, b = Regression.findRegression(xList, yList)
+        f1, f2, f3 = percepNum.feature(x)
+        featureList  = [f1, f2, f3]
         equationList = [0,0,0,0,0,0,0,0,0,0]
         for a in range(10):
-            equationList[a] = weightsList[a][0] + weightsList[a][1]*m
+            equationList[a] = weightsList[a][0] + weightsList[a][1]*f1 + weightsList[a][2]*f2 + weightsList[a][3]*f3
         largestEquation = equationList[0]
         equationNum = 0
         for q in range(10):
@@ -47,10 +47,19 @@ class percepNum:
             #weightsList[equationNum][0] = weightsList[equationNum][0] - 1
             for k in range(j):
                 if k>0:
-                    weightsList[equationNum][k] = weightsList[equationNum][k] - m
-                    weightsList[int(x.label)][k] = weightsList[int(x.label)][k] + m
+                    weightsList[equationNum][k] = weightsList[equationNum][k] - featureList[k-1]
+                    weightsList[int(x.label)][k] = weightsList[int(x.label)][k] + featureList[k-1]
 
         return weightsList, numPass
+
+    @staticmethod
+    def feature(x):
+        xList, yList = Regression.makeLists(x.image)
+        m, b = Regression.findRegression(xList, yList)
+        v = Gap.vertical(x.image)
+        h = Gap.horizontal(x.image)
+        return m, v, h
+
     def buildPhi(self):  # number of + and # over total pixels in line
         for node in self.samples:
             node.phiVector = {
@@ -62,12 +71,11 @@ class percepNum:
             node.phiVector['m'] = round(m, 1)
 
     def predict(self, x, weightsList):
-        xList, yList = Regression.makeLists(x.image)
-        m, b = Regression.findRegression(xList, yList)
+        f1, f2, f3 = percepNum.feature(x)
         equationList = [0,0,0,0,0,0,0,0,0,0]
         rand = random.randint(0,9)
         for a in range(10):
-            equationList[a] = weightsList[a][0] + weightsList[a][1] * m
+            equationList[a] = weightsList[a][0] + weightsList[a][1] *f1 + weightsList[a][2]*f2 + weightsList[a][3]*f3
         largestEquation = equationList[0]
         equationNum = 0
         for q in range(10):
@@ -78,16 +86,16 @@ class percepNum:
 
 if __name__ == '__main__':
 
-    weights0 = [0.01,0.01]
-    weights1 = [0.01,0.01]
-    weights2 = [0.01,0.01]
-    weights3 = [0.01,0.01]
-    weights4 = [0.01,0.01]
-    weights5 = [0.01,0.01]
-    weights6 = [0.01,0.01]
-    weights7 = [0.01,0.01]
-    weights8 = [0.01,0.01]
-    weights9 = [0.01,0.01]
+    weights0 = [0.01,0.01,0.01,0.01]
+    weights1 = [0.01,0.01,0.01,0.01]
+    weights2 = [0.01,0.01,0.01,0.01]
+    weights3 = [0.01,0.01,0.01,0.01]
+    weights4 = [0.01,0.01,0.01,0.01]
+    weights5 = [0.01,0.01,0.01,0.01]
+    weights6 = [0.01,0.01,0.01,0.01]
+    weights7 = [0.01,0.01,0.01,0.01]
+    weights8 = [0.01,0.01,0.01,0.01]
+    weights9 = [0.01,0.01,0.01,0.01]
     weightsList = [weights0, weights1, weights2, weights3, weights4, weights5, weights6, weights7, weights8, weights9]
     j = len(weights0)
     digitHeight = 28
@@ -109,7 +117,7 @@ if __name__ == '__main__':
     endPoint = 0
     numPass = 0
     forceEnd = 0
-    while (endPoint ==0 or endPoint > numPass) and forceEnd < 100:
+    while (endPoint == 0 or endPoint > numPass) and forceEnd < 500:
 
         forceEnd = forceEnd + 1
         #print(forceEnd)
